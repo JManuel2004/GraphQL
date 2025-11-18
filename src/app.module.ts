b0bec5c302c249d938032c6f63527dc3bb5c2a35
Jmanuel2004 <manxicardona@gmail.com>
2025-11-18 11:31:15 -0500

    feat: Preparar para despliegue en Render (Dockerfile, config DB)

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,11 +39,7 @@ import { SeedModule } from './seed/seed.module';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        url: configService.get('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: true, 
         logging: false,
